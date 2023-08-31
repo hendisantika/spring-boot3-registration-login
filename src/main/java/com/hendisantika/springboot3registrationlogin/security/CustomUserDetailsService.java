2774463a5f88234136f93fdad86f45750975655b
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:53:34 +0700

    #21 - Add mapRolesToAuthorities in CustomUserDetailsService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3registrationlogin/security/CustomUserDetailsService.java
+++ src/main/java/com/hendisantika/springboot3registrationlogin/security/CustomUserDetailsService.java
@@ -1,12 +1,18 @@
 package com.hendisantika.springboot3registrationlogin.security;
 
+import com.hendisantika.springboot3registrationlogin.entity.Role;
 import com.hendisantika.springboot3registrationlogin.entity.User;
 import com.hendisantika.springboot3registrationlogin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +41,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         } else {
             throw new UsernameNotFoundException("Invalid username or password.");
         }
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        Collection<? extends GrantedAuthority> mapRoles = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return mapRoles;
     }
 }
