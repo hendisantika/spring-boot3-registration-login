0cc867d0e52e6c22c1761db2c1b6cdc6b3b9cfe9
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:46:01 +0700

    #4 - Add Role Entity ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3registrationlogin/entity/Role.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springboot3registrationlogin.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-registration-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/31/23
+ * Time: 08:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
