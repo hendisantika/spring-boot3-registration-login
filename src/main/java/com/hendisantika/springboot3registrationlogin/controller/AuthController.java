2e68245c4396ddd24d4019acb06b787c2b24724d
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:55:16 +0700

    #26 - Add registration in AuthController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3registrationlogin/controller/AuthController.java
+++ src/main/java/com/hendisantika/springboot3registrationlogin/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.hendisantika.springboot3registrationlogin.controller;
 
 import com.hendisantika.springboot3registrationlogin.dto.UserDto;
+import com.hendisantika.springboot3registrationlogin.entity.User;
 import com.hendisantika.springboot3registrationlogin.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,5 +43,22 @@ public class AuthController {
         UserDto user = new UserDto();
         model.addAttribute("user", user);
         return "register";
+    }
+
+    // handler method to handle register user form submit request
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto user,
+                               BindingResult result,
+                               Model model) {
+        User existing = userService.findByEmail(user.getEmail());
+        if (existing != null) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+        userService.saveUser(user);
+        return "redirect:/register?success";
     }
 }
