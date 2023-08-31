ef24f4f0ff5f93c362348ad3b62e1cba040d5ae9
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 09:16:52 +0700

    #32 - Bug Fix issues ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3registrationlogin/entity/User.java
+++ src/main/java/com/hendisantika/springboot3registrationlogin/entity/User.java
@@ -41,7 +41,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinTable(
             name = "users_roles",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
