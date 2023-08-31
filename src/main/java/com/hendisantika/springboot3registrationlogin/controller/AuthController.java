4cdfc0b5aba508e5cf5c54d13a3b9c2ecf919d1d
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:54:17 +0700

    #22 - Add AuthController ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3registrationlogin/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springboot3registrationlogin.controller;
+
+import com.hendisantika.springboot3registrationlogin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-registration-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/31/23
+ * Time: 08:53
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+}
