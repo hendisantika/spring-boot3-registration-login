50ac0f95c64bffb34c6ccca29dcd37be78224e7f
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:51:25 +0700

    #16 - Add passwordEncoder in SpringSecurity ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3registrationlogin/config/SpringSecurity.java
+++ src/main/java/com/hendisantika/springboot3registrationlogin/config/SpringSecurity.java
@@ -1,9 +1,12 @@
 package com.hendisantika.springboot3registrationlogin.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +25,8 @@ public class SpringSecurity {
 
     private final UserDetailsService userDetailsService;
 
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
