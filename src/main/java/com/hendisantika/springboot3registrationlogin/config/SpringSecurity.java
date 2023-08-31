e0502c4f0083729996407974529c0bc49b0c1676
hendisantika <hendisantika@yahoo.co.id>
2023-08-31 08:52:01 +0700

    #18 - Add configureGlobal in SpringSecurity ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3registrationlogin/config/SpringSecurity.java
+++ src/main/java/com/hendisantika/springboot3registrationlogin/config/SpringSecurity.java
@@ -1,8 +1,10 @@
 package com.hendisantika.springboot3registrationlogin.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,6 +33,13 @@ public class SpringSecurity {
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
